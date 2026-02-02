871d0aa2ad52bd25b17dd9ddf4881572bef80adc
Renato Martins <guillenmartins@gmail.com>
2026-02-02 01:18:54 +0000

    Refactor code structure for improved readability and maintainability

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,23 @@
+@props([
+    'title' => 'Content approval',
+])
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ $title }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+
+    <div class="container mb-5">
+        {{ $slot }}
+    </div>
+</body>
+
+</html>
