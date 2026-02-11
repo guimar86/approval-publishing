a5eff4979677af1d79a472317fee64e522058de0
Renato Martins <guillenmartins@gmail.com>
2026-02-11 09:52:07 +0000

    Alpinejs library added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,9 @@
 import './bootstrap';
+
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import './../css/index.css';
+import Alpine from 'alpinejs';
+
+Alpine.start();
