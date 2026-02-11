40dd40286de9402b204f9992ec1969730f03196b
Renato Martins <guillenmartins@gmail.com>
2026-02-11 09:53:00 +0000

    Change redirection after logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -81,7 +81,7 @@ class AuthController extends Controller
     public function destroy(User $user)
     {
         Auth::logout();
-        return redirect()->route("auth.login");
+        return redirect('/');
     }
 }
 
